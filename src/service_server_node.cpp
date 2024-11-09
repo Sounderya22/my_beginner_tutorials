2198407e542ad099d3b111e20deb295fd7ebd7e5
Sounderya22 <vvsounderya22@gmail.com>
2024-11-08 23:04:39 -0500

    Added MIT License

--- src/service_server_node.cpp
+++ src/service_server_node.cpp
@@ -2,10 +2,29 @@
  * @file trigger_service_server.cpp
  * @brief Implementation of a Trigger service server node in ROS 2 using rclcpp.
  *
- * This file defines a simple ROS 2 service server node that provides a service
- * called "trigger_service". The service returns a success message to any client
- * that calls it.
+ * MIT License
+ * 
+ * Copyright (c) [year] [your name or organization]
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
+
 
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/srv/trigger.hpp"
